9a44bc0592b5c0f526e4d931b92b1a99d23f86b6
zqh <zqingheng@163.com>
2017-12-13 17:32:13 +0800

    add full stack

--- train_cnn.py
+++ train_cnn.py
@@ -3,7 +3,7 @@ import os
 from gensim.models.keyedvectors import KeyedVectors
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam
-
+from utils import cluster_quality
 EMBEDDING_FILE = 'data/GoogleNews-vectors-negative300.bin'
 
 
@@ -11,10 +11,10 @@ EMBEDDING_FILE = 'data/GoogleNews-vectors-negative300.bin'
 text_path = 'data/StackOverflow.txt'
 label_path = 'data/StackOverflow_gnd.txt'
 
-with open(text_path, encoding="utf-8") as f:
+with open(text_path) as f:
     data = [text.strip() for text in f]
 
-with open(label_path, encoding="utf-8") as f:
+with open(label_path) as f:
     target = f.readlines()
 target = [int(label.rstrip('\n')) for label in target]
 
@@ -51,7 +51,6 @@ word2vec = KeyedVectors.load_word2vec_format(EMBEDDING_FILE, binary=True)
 
 EMBEDDING_DIM = 300
 nb_words = min(MAX_NB_WORDS, len(word_index)) + 1
-
 embedding_matrix = np.zeros((nb_words, EMBEDDING_DIM))
 for word, i in word_index.items():
     if word in word2vec.vocab:
@@ -93,23 +92,22 @@ from keras.layers import Dense, Conv1D, Dropout, merge
 from keras.layers import MaxPooling1D, GlobalMaxPooling1D, ZeroPadding1D
 from keras.models import Model
 
-embedding_matrix_copy = embedding_matrix.copy()
-trainable_embedding = False
-
-# Embedding layer
-pretrained_embedding_layer = Embedding(
-    input_dim=nb_words,
-    output_dim=EMBEDDING_DIM,
-    weights=[embedding_matrix],
-    input_length=MAX_SEQUENCE_LENGTH,
-)
-
-# Input
-sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
-embedded_sequences = pretrained_embedding_layer(sequence_input)
-
 
 def get_model():
+    embedding_matrix_copy = embedding_matrix.copy()
+    trainable_embedding = False
+    # Embedding layer
+    pretrained_embedding_layer = Embedding(
+        input_dim=nb_words,
+        output_dim=EMBEDDING_DIM,
+        weights=[embedding_matrix],
+        input_length=MAX_SEQUENCE_LENGTH,
+    )
+
+    # Input
+    sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+    embedded_sequences = pretrained_embedding_layer(sequence_input)
+    
     # 1st Layer
     x = Conv1D(100, 5, activation='tanh', padding='same')(embedded_sequences)
     x = GlobalMaxPooling1D()(x)
@@ -119,22 +117,46 @@ def get_model():
     predictions = Dense(TARGET_DIM, activation='sigmoid')(x)
     model = Model(sequence_input, predictions)
 
-    # Fine-tune embeddings or not
     model.layers[1].trainable=trainable_embedding
 
-    checkpoint = ModelCheckpoint('models/weights.{epoch:03d}-{val_acc:.4f}.hdf5', monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
     adam = Adam(lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
     # Loss and Optimizer
     model.compile(loss='binary_crossentropy',
                   optimizer=adam,
                   metrics=['mae'])
-
+    # Fine-tune embeddings or not
     model.summary()
     return model
 
-nb_epoch = 50
-model = get_model()
-model.fit(X, B, validation_split=0.2,
-          epochs=nb_epoch, batch_size=100, verbose=2, shuffle=True, callbacks=[checkpoint])
+if __name__ == '__main__':
+    nb_epoch = 50
+    checkpoint = ModelCheckpoint('models/weights.{epoch:03d}-{val_acc:.4f}.hdf5', monitor='val_acc', verbose=1, save_best_only=True, mode='auto')
+    model = get_model()
+    model.fit(X, B, validation_split=0.2,
+              epochs=nb_epoch, batch_size=100, verbose=1, shuffle=True)
+
+    # create model that gives penultimate layer
+    input = model.layers[0].input
+    output = model.layers[-2].output
+    model_penultimate = Model(input, output)
+
+    # inference of penultimate layer
+    H = model_penultimate.predict(X)
+    print("Sample shape: {}".format(H.shape))
 
 
+    from sklearn.preprocessing import normalize
+    from sklearn.cluster import KMeans
+
+    true_labels = y
+    n_clusters = len(np.unique(y))
+    print("Number of classes: %d" % n_clusters)
+    km = KMeans(n_clusters=n_clusters, n_jobs=10)
+    result = dict()
+    V = normalize(H, norm='l2')
+    km.fit(V)
+    pred = km.labels_
+    print(pred)
+    a = {'deep': cluster_quality(true_labels, pred)}
+    np.save("pred.npy", pred)
+    model.save_weights("model.plk")
