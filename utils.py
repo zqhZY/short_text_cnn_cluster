c2f072aec14e8a77446e3ddca67020c3c34e4281
zqh <zqingheng@163.com>
2017-12-13 17:29:21 +0800

    add cluster eval

--- utils.py
+++ utils.py
@@ -1,7 +1,50 @@
 import numpy as np
+from collections import Counter
+from operator import itemgetter
+from sklearn import metrics
+
 
 def binarize(target):
     median = np.median(target, axis=1)[:, None]
     binary = np.zeros(shape=np.shape(target))
     binary[target > median] = 1
     return binary
+
+
+def map_label(true_labels, pred_labels):
+    label_pair = list(zip(pred_labels, true_labels))
+    count = tuple(Counter(label_pair).items())
+    mapping = dict()
+    n_label = len(np.unique(true_labels))
+
+    # map most likely labels from prediction to ground truth
+    for label in range(n_label):
+        tuples = [tup for tup in count if tup[0][0] == label]
+        likely_tuple = max(tuples, key=itemgetter(1))[0]
+        mapping[likely_tuple[0]] = likely_tuple[1]
+
+    pred_labels_mapped = [mapping[x] for x in pred_labels]
+    return pred_labels_mapped
+
+
+def cluster_quality(true_labels, pred_labels, show=True):
+    h, c, v = metrics.homogeneity_completeness_v_measure(true_labels, pred_labels)
+    nmi = metrics.normalized_mutual_info_score(true_labels, pred_labels)
+    rand = metrics.adjusted_rand_score(true_labels, pred_labels)
+    pred_labels_mapped = map_label(true_labels, pred_labels)
+    acc = metrics.accuracy_score(true_labels, pred_labels_mapped)
+    if show:
+        print("Homogeneity: %0.3f" % h)
+        print("Completeness: %0.3f" % c)
+        print("V-measure: %0.3f" % v)
+        print("NMI: %0.3f" % nmi)
+        print("Rand score: %0.3f" % rand)
+        print("Accuracy: %0.3f" % acc)
+    return dict(
+        homogeneity=h,
+        completeness=c,
+        vmeasure=v,
+        nmi=nmi,
+        rand=rand,
+        accuracy=acc,
+    )
